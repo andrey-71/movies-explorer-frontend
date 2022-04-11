c311cce3178b6e72e6cafad105e0f9fd4805845a
andrey-71 <and_go1991@mail.ru>
2022-04-11 14:02:02 +0300

    feat: сверстать компонент Techs (+адаптив)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,15 @@
 import './Main.css';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+
 function Main() {
 
   return (
     <>
       <Promo />
       <AboutProject />
+      <Techs />
     </>
   )
 }
---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,26 @@
+import './Techs.css';
+
+function Techs() {
+
+  return(
+    <section className='techs'>
+      <h2 className='techs__title'>Технологии</h2>
+      <h3 className='techs__text-title'>7 технологий</h3>
+      <p className='techs__text-description'>
+        На курсе веб-разработки мы освоили технологии,
+        которые применили в дипломном проекте.
+      </p>
+      <ul className='techs__stack'>
+        <li className='techs__stack-item'>HTML</li>
+        <li className='techs__stack-item'>CSS</li>
+        <li className='techs__stack-item'>JS</li>
+        <li className='techs__stack-item'>React</li>
+        <li className='techs__stack-item'>Git</li>
+        <li className='techs__stack-item'>Express.js</li>
+        <li className='techs__stack-item'>mongoDB</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Techs;

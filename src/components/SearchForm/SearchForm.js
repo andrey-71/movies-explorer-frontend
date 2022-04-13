ac6fd67472d549c4b1a63b8897aa97672bb33357
andrey-71 <and_go1991@mail.ru>
2022-04-13 16:12:23 +0300

    feat: сверстать компонент SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,11 +6,14 @@ function SearchForm() {
   return (
     <section className='search-form'>
       <div className='search-form__container'>
-        <div className='search-form__icon' />
-        <input type='text' placeholder='Фильмы' className='search-form__input'/>
-        <button className='search-form__button'></button>
-        <FilterCheckbox />
-
+        <div className='search-form__search-container'>
+          <div className='search-form__icon' />
+          <input type='text' placeholder='Фильм' className='search-form__input' />
+          <button className='search-form__button'>
+            <span className='search-form__button-icon' />
+          </button>
+        </div>
+        <FilterCheckbox description='Короткометражки' />
       </div>
     </section>
   )

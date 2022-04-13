f5b92149f11c58a22d2a57db4baea2bfd0237d74
andrey-71 <and_go1991@mail.ru>
2022-04-13 11:34:37 +0300

    refactor: внести текст с описанием чекбокса в компонент FilterCheckbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,14 +1,18 @@
 import './FilterCheckbox.css';
 
-function FilterCheckbox() {
+function FilterCheckbox(props) {
 
   return (
-    <label htmlFor='filterCheckbox' className='filter-checkbox'>
-      <input type='checkbox' id='filterCheckbox' className='filter-checkbox__checkbox'/>
-      <span className='filter-checkbox__custom-checkbox'>
+    <div className='filter-checkbox'>
+      <label htmlFor='filterCheckbox' className='filter-checkbox__container'>
+        <input type='checkbox' id='filterCheckbox' className='filter-checkbox__checkbox'/>
+        <span className='filter-checkbox__custom-checkbox'>
         <i className='filter-checkbox__circle' />
       </span>
-    </label>
+      </label>
+      <p className='filter-checkbox__description'>{props.description}</p>
+    </div>
+
   )
 }
 export default FilterCheckbox;

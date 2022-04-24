8ced0e64871c0c209043bfb9d181a4ecf36ae9b6
andrey-71 <and_go1991@mail.ru>
2022-04-24 16:44:41 +0300

    fix: привести всплывающее меню в соответствие с макетом - максимальная ширина 520px, расположение справа, добавить плавное появление контента справа

--- src/components/MenuBurger/MenuBurger.js
+++ src/components/MenuBurger/MenuBurger.js
@@ -6,15 +6,22 @@ function MenuBurger(props) {
     :
     'menu-burger';
 
+  const menuBurgerContainerClassName = props.isActive ?
+    'menu-burger__container menu-burger__container_active'
+    :
+    'menu-burger__container';
+
   return(
-    <nav className={menuBurgerClassName}>
-      <div className='menu-burger__films'>
-        <a href='#' className='menu-burger__link'>Главная</a>
-        <a href='#' className='menu-burger__link'>Фильмы</a>
-        <a href='#' className='menu-burger__link'>Сохраненные фильмы</a>
-      </div>
-      <a href='#' className='menu-burger__link menu-burger__link_account'>Аккаунт</a>
-    </nav>
+    <div className={menuBurgerClassName}>
+      <nav className={menuBurgerContainerClassName}>
+        <div className='menu-burger__films'>
+          <a href='#' className='menu-burger__link'>Главная</a>
+          <a href='#' className='menu-burger__link'>Фильмы</a>
+          <a href='#' className='menu-burger__link'>Сохраненные фильмы</a>
+        </div>
+        <a href='#' className='menu-burger__link menu-burger__link_account'>Аккаунт</a>
+      </nav>
+    </div>
   )
 }
 

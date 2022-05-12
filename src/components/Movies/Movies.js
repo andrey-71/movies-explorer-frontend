9b3c43ac3472a174fc46571e6a011dc52b501839
andrey-71 <and_go1991@mail.ru>
2022-05-12 09:06:24 +0300

    feat: реализовать хук с обработчиком события изменения ширины экрана

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,5 +1,5 @@
 import './Movies.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import AddMovies from '../AddMovies/AddMovies';
@@ -10,6 +10,34 @@ function Movies() {
   const [movies, setMovies] = useState([]);
   // Стейт данных в поисковой строке
   const [dataSearch, setDataSearch] = useState('');
+  // Количество отрисовываемых карточек
+  const [renderInitialCardNumber, setRenderInitialCardNumber] = useState(null);
+  const [renderMoreCardNumber, setRenderMoreCardNumber] = useState(null);
+
+  // Начальная загрузка карточек, запись в localStorage
+
+
+  // Установка обработчика отслеживания размеров экрана
+  useEffect(() => {
+    const handleScreenWidth = () => {
+      if (document.documentElement.scrollWidth >= 1280) {
+        setRenderInitialCardNumber(9);
+        setRenderMoreCardNumber(3);
+      }
+      else if (document.documentElement.scrollWidth >= 728) {
+        setRenderInitialCardNumber(6);
+        setRenderMoreCardNumber(2);
+      }
+      else {
+        setRenderInitialCardNumber(5);
+      }
+    };
+
+    window.addEventListener("resize", handleScreenWidth);
+    return () => window.removeEventListener("resize", handleScreenWidth);
+  }, [renderInitialCardNumber, renderMoreCardNumber]);
+
+  console.log(renderInitialCardNumber, renderMoreCardNumber);
 
   // Функция обработки результатов поиска
   function checkMatchMovie(movies) {

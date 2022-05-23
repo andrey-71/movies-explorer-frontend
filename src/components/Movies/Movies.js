bdf0360dd08ab4c46b81990459ce13982c918d06
andrey-71 <and_go1991@mail.ru>
2022-05-23 17:46:54 +0300

    feat: совместить функции по поиску и рендерингу фильмов, изпользовать прелоадер при загрузке страницы, брать найденные фильмы и фильмы для рендеринга из localStorage, при наличии

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -6,6 +6,8 @@ import AddMovies from '../AddMovies/AddMovies';
 import moviesApi from "../../utils/MoviesApi";
 
 function Movies() {
+  // Все фильмы
+  const [allMovies, setAllMovies] = useState([]);
   // Найденные при поиске карточки
   const [foundMovies, setFoundMovies] = useState([]);
   // Карточки с фильмами для рендеринга
@@ -23,21 +25,30 @@ function Movies() {
   // Установка кол-ва отрисовываемых (добавляемых) карточек в зависимости от ширины экрана
   useEffect(() => {
     handleScreenWidth();
+    setRenderMovies(JSON.parse(localStorage.getItem('renderMovies')));
+    setFoundMovies(JSON.parse(localStorage.getItem('foundMovies')));
+    // if (foundMovies && foundMovies.length >= renderInitialCardNumber) {
+    //   console.log(new Boolean(renderInitialCardNumber));
+    //   console.log(new Boolean(foundMovies));
+    //   setIsAddButton(true);
+    // }
   }, []);
 
   // Установка обработчика отслеживания размеров экрана
   useEffect(() => {
-    window.addEventListener("resize", handleScreenWidth);
-    return () => window.removeEventListener("resize", handleScreenWidth);
+    window.addEventListener("resize", () => setTimeout(handleScreenWidth, 500));
+    return () => window.removeEventListener("resize", () => setTimeout(handleScreenWidth, 500));
   }, [renderInitialCardNumber, renderMoreCardNumber]);
 
-  // Запись всех карточек в localStorage,
+  // Загрузка вех карточек
   useEffect(() => {
+    setIsPreloader(true);
       moviesApi.getMovies()
         .then((movies) => {
-          localStorage.setItem('allMovies', JSON.stringify(movies));
+          setAllMovies(movies);
         })
         .catch(err => console.log(err))
+        .finally(() => setIsPreloader(false));
   }, []);
 
   // Функция добавления фильмов при нажатии кнопки ещё из списка результатов поиска
@@ -45,7 +56,6 @@ function Movies() {
     const renderMoviesList = [];
     foundMovies.map(movie => {
       if (foundMovies.indexOf(movie) < renderMovies.length + renderMoreCardNumber) {
-        console.log(foundMovies.indexOf(movie));
         renderMoviesList.push(movie);
         if (foundMovies.indexOf(movie) === foundMovies.length - 1) {
           setIsAddButton(false);
@@ -55,37 +65,31 @@ function Movies() {
     setRenderMovies(renderMoviesList);
   }
 
-  // Функция создания списка фильмов для первичного рендеринга
-  function renderingMoviesList(movies) {
-    const renderMoviesList = [];
-    movies.length !== 0 &&
-    movies.map(movie => {
-      if (movies.indexOf(movie) < renderInitialCardNumber) {
-        renderMoviesList.push(movie);
-      }
-    })
-    if (foundMovies.length >= renderInitialCardNumber) {
-      setIsAddButton(true);
-    }
-    setRenderMovies(renderMoviesList);
-  }
-
-  // Функция обработки результатов поиска
-  function checkMatchMovie(movies) {
-    const foundMoviesList = movies.filter(movie => movie.nameRU.indexOf(dataSearch) >= 0);
-    foundMoviesList.length !==0 && setFoundMovies(foundMoviesList);
-  }
-
-  // Получение фильмов и обработка результатов поиска
+  // Функция поиска фильмов и рендеринга на странице
   function searchMovies(e) {
     e.preventDefault();
     if (dataSearch.length !== 0) {
       try {
-        const allMoviesList = JSON.parse(localStorage.allMovies);
-        checkMatchMovie(allMoviesList);
-        renderingMoviesList(foundMovies);
-      } catch (err) {
-        console.log('Ничего не найдено');
+        // Обработка результатов поиска
+        const foundMoviesList = allMovies.filter(movie => movie.nameRU.toLowerCase().indexOf(dataSearch.toLowerCase()) >= 0);
+        localStorage.setItem('foundMovies', (JSON.stringify(foundMoviesList)));
+        // Рендеринг карточек фильмов
+        const renderMoviesList = [];
+        if (foundMoviesList.length !== 0) {
+          foundMoviesList.map(movie => {
+            foundMoviesList.indexOf(movie) < renderInitialCardNumber && renderMoviesList.push(movie);
+          })
+          localStorage.setItem('renderMovies', (JSON.stringify(renderMoviesList)));
+          foundMoviesList.length >= renderInitialCardNumber && setIsAddButton(true);
+          // Запись в стейт найденных и отрендеринных фильмов
+          setFoundMovies(foundMoviesList);
+          setRenderMovies(renderMoviesList);
+        } else {
+          console.log('Ничего не найдено');
+        }
+      }
+      catch (err) {
+        console.log('Произошла ошибка');
       }
     } else {
       console.log('Введите название фильма');
@@ -120,7 +124,7 @@ function Movies() {
         renderInitialCardNumber={renderInitialCardNumber}
         renderMoreCardNumber={renderMoreCardNumber}
       />
-      {isAddButton ?
+      {isAddButton || isPreloader ?
         <AddMovies
           isPreloaderVisible={isPreloader}
           onClick={handleAddMovies}

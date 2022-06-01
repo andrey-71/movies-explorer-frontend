ab124a91a77a33ea62c2a28e988bb4babb3a9e69
andrey-71 <and_go1991@mail.ru>
2022-06-01 19:11:16 +0300

    refactor: прописать данные фильмов в нужных местах компонента

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,53 +1,11 @@
 import './MoviesCard.css';
-import { useState, useEffect } from 'react';
-import { serverUrl } from '../../utils/config';
-import mainApi from '../../utils/MainApi';
 
 function MoviesCard(props) {
-  const [isSaved, setIsSaved] = useState(false);
-  const buttonMoviesClassName = !isSaved ?
-    'card__saved-button card__saved-button_type_movies'
-    :
-    'card__saved-button card__saved-button_type_movies card__saved-button_active';
-  const buttonSavedMoviesClassName = 'card__saved-button card__saved-button_type_saved-movies';
-
-  // Определение сохраненных фильмов
-  useEffect(() => {
-    if (!props.isPageSavedMovies) {
-      mainApi.getSavedMovies()
-        .then(savedMovies => {
-          if (savedMovies) {
-            savedMovies.some(savedMovie => {
-              savedMovie.movieId === props.movie.id && setIsSaved(true);
-            })
-          }
-        })
-        .catch(err => console.log(`При загрузке фильмов произошла ошибка: ${err}`));
-    }
-  }, []);
-
-  console.log(isSaved);
-
-  // Функция сохранения фильма
-  function handleAddSavedMovies(data) {
-    mainApi.addSavedMovies(data)
-      .then(() => {
-        setIsSaved(true);
-        console.log(true);
-      })
-      .catch(err => console.log(`При сохранении фильма произошла ошибка: ${err}`));
-  }
-
-    // Функция сохранения фильмов
-  function handleSavedMovie(evt) {
-    evt.stopPropagation();
-    !isSaved && handleAddSavedMovies(props.movie);
-  }
 
   // Функция для перевода времени
   function calcDuration() {
-    const hours = props.movie.duration/60;
-    const minutes = props.movie.duration%60;
+    const hours = props.duration/60;
+    const minutes = props.duration%60;
     let resultDuration = '';
     if (parseInt(hours) > 0) { resultDuration += `${parseInt(hours)}ч ` }
     if (minutes > 0) { resultDuration += `${minutes}м` }
@@ -58,25 +16,15 @@ function MoviesCard(props) {
 
   return (
     <article className='card'>
-      <a className='card__container' href={props.movie.trailerLink} target='_blank' />
+      <a className='card__container' href={props.trailerLink} target='_blank' />
       <div className='card__container-info'>
-        <h2 className='card__title'>{props.movie.nameRU}</h2>
+        <h2 className='card__title'>{props.name}</h2>
         <p className='card__duration'>{durationMovies}</p>
-        {!props.isPageSavedMovies ?
-          <button
-            className={buttonMoviesClassName}
-            onClick={handleSavedMovie}
-          />
-          :
-          <button
-            className={buttonSavedMoviesClassName}
-          />
-        }
+        {props.children}
       </div>
       <img
         className='card__image'
-        src={props.isPageSavedMovies ?
-          props.movie.image : serverUrl.imageMovies + props.movie.image.url}
+        src={props.imageLink}
         alt='Изображение-превью к фильму'
       />
     </article>
